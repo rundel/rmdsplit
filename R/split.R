7d948b44309aa947efc684df0b2cdd9ca775ad26
Colin Rundel <rundel@gmail.com>
2020-10-30 20:09:42 +0000

    Borked

--- R/split.R
+++ R/split.R
@@ -1,11 +1,78 @@
+make_chunk_obj = function(
+  engine = "r", name = "",
+  options = structure(list(), names = character()),
+  code = character(),
+  indent = ""
+) {
+  structure(
+    list(
+      engine = engine, name = name, options = options, code = code, indent = indent
+    ),
+    class = "rmd_chunk"
+  )
+}
+
+find_splits = function(rmd, headings) {
+  secs = parsermd::rmd_node_sections(rmd, levels = headings, drop_na = TRUE)
+  sec_ids = match(secs, unique(secs))
+  sec_id_counts = table(sec_ids)
+
+  content = sec_ids %in% which(sec_id_counts > 1) & # Sections with content
+    purrr::map_lgl(secs, ~ length(.x) != 0)         # Not preamble
+
+  splits = secs[content]
+
+  splits[!duplicated(splits)]
+}
+
+
+deparse( quote(
+  local({
+    hook_error = knitr::knit_hooks$get("error")
+    knitr::knit_hooks$set(error = function(x, options) {
+      if (isTRUE(options$preamble)) x = character()
+      hook_error(x, options)
+    })
+  })
+) )
+
+
+knitr_hook_chunk = function() {
+  structure(
+    list(
+      engine = "r",
+      name = "knitr-hook-chunk",
+      options = list(include = FALSE),
+      code = deparse( quote(
+        local({
+          hook_error = knitr::knit_hooks$get("error")
+          knitr::knit_hooks$set(error = function(x, options) {
+            if (isTRUE(options$preamble)) x = character()
+            hook_error(x, options)
+          })
+        })
+      ) ),
+      indent = ""
+    ),
+    class = "rmd_chunk"
+  )
+}
+
+
+
 #' @export
 rmd_split_render = function(
-  file, render_func, headings = 1:6, out_dir,
-  prefix = "split_", ext = "pdf"
+  file, template = file,
+  render_func = wkhtml_renderer,
+  headings = 1:6, out_dir,
+  prefix = "split_", ext = "pdf",
+  force_allow_errors = FALSE
 ) {
   stopifnot(fs::dir_exists(out_dir))
 
-  file = fs::path_abs(file)
+  file = fs::path_real(file)
+  out_dir = fs::path_real(out_dir)
+  template = fs::path_real(template)
 
   dir = fs::path_dir(file)
   file = fs::path_file(file)
@@ -14,20 +81,42 @@ rmd_split_render = function(
 
 
   rmd = parsermd::parse_rmd(file)
-  rmd_tbl = parsermd::as_tibble(rmd)
+  rmd_tmpl = parsermd::parse_rmd(template)
+
+  # FIXME dirty hack for now
+  rmd[[1]]$subtitle = fs::path_file(dir)
 
   secs = parsermd::rmd_node_sections(rmd, levels = headings, drop_na = TRUE)
-  sec_ids = match(secs, unique(secs))
-  sec_id_counts = table(sec_ids)
 
-  content = sec_ids %in% which(sec_id_counts > 1) & # Sections with content
-    purrr::map_lgl(secs, ~ !all(is.na(.x))) # Not preamble
+  splits = find_splits(rmd_tmpl, headings)
 
-  splits = secs[content]
-  splits = splits[!duplicated(splits)]
+  pb = progress::progress_bar$new(
+    total = length(splits) + 1,
+    format = ":what [:bar] :percent eta: :eta"
+  )
 
-  for(i in seq_along(splits)) {
 
+  # Create a title page with yaml details
+
+  rmd_file = withr::local_tempfile(tmpdir = getwd(), fileext = ".Rmd")
+
+  rmd %>%
+    parsermd::rmd_subset(type_refs = c("rmd_yaml","rmd_yaml_list"))  %>%
+    parsermd::as_document() %>%
+    paste(collapse="\n") %>%
+    readr::write_file(rmd_file)
+
+  output = file.path(
+    out_dir,
+    paste0(prefix, 0, ".", ext)
+  )
+
+  pb$tick(tokens = list(what = fs::path_file(output)))
+
+  render_func(rmd_file,  output)
+
+  # Work through the splits, one output per
+  for(i in 10:12) {#seq_along(splits)) {
     split = splits[i]
 
     start = match(split, secs)
@@ -56,6 +145,39 @@ rmd_split_render = function(
     cur_sec = rmd %>%
       parsermd::rmd_subset(sec_refs = split)
 
+    if (force_allow_errors) {
+      preamble = purrr::map(
+        preamble,
+        function(node) {
+          if (inherits(node, "rmd_chunk")) {
+            # include = FALSE w/ error = TRUE does not work
+            node$options$error = TRUE
+            node$options$include = TRUE
+            node$options$echo = FALSE
+            node$options$warning = FALSE
+            node$options$message = FALSE
+            node$options$results = '"asis"'
+            node$options$preamble = TRUE
+          }
+          node
+        }
+      ) %>%
+        c(list(knitr_hook_chunk()), .) %>%
+        structure(class = c("rmd_ast", "list"))
+
+      cur_sec = purrr::map(
+        cur_sec,
+        function(node) {
+          if (inherits(node, "rmd_chunk")) {
+            node$options$error = TRUE
+            node$options$include = TRUE
+          }
+          node
+        }
+      ) %>%
+        structure(class = c("rmd_ast", "list"))
+    }
+
     split_ast = structure(
       c(preamble, cur_sec),
       class = c("rmd_ast", "list")
@@ -65,7 +187,7 @@ rmd_split_render = function(
       paste(collapse="\n")
 
     try({
-      rmd_file = withr::local_tempfile(tmpdir = getwd(), fileext = ".Rmd")
+      rmd_file = glue::glue("test{i}.Rmd") #withr::local_tempfile(tmpdir = getwd(), fileext = ".Rmd")
 
       readr::write_file(doc, rmd_file)
 
@@ -74,23 +196,10 @@ rmd_split_render = function(
         paste0(prefix, i, ".", ext)
       )
 
+      pb$tick(tokens = list(what = fs::path_file(output)))
       render_func(rmd_file,  output)
     })
   }
-
-  rmd_file = withr::local_tempfile(tmpdir = getwd(), fileext = ".Rmd")
-
-  rmd %>%
-    parsermd::rmd_subset(type_refs = c("rmd_yaml","rmd_yaml_list"))  %>%
-    parsermd::as_document() %>%
-    paste(collapse="\n") %>%
-    readr::write_file(rmd_file)
-
-  output = file.path(
-    out_dir,
-    paste0(prefix, 0, ".", ext)
-  )
-  render_func(rmd_file,  output)
 
   invisible()
 }
@@ -110,23 +219,24 @@ rmd_split_render = function(
 #)
 
 #' @export
-wkhtml_renderer = function(file, out) {
+wkhtml_renderer = function(file, out, debug = FALSE) {
   html_out = withr::local_tempfile(tmpdir = getwd(), fileext = ".html")
 
   # This is nonsense but needed to shut pandoc up
-  out_fmt = rmarkdown::html_document()
-  out_fmt$pandoc$args = c(out_fmt$pandoc$args, "--metadata", "pagetitle=blah")
+  out_fmt = rmarkdown::html_document(keep_md=TRUE)
+  out_fmt$pandoc$args = c(out_fmt$pandoc$args, "--metadata", "pagetitle=wkhtml")
 
   rmarkdown::render(
     file,
     output_file = html_out,
     output_format = out_fmt,
-    quiet = TRUE
+    quiet = FALSE #!debug
   )
 
   processx::run(
     "wkhtmltopdf",
-    c("-d", "200", html_out, out)
+    c("-d", "200", html_out, out),
+    echo_cmd = debug
   )
 }
 
